8124dc36119a7220f92324a3ccb6fa7050a981fa
Gerardo Gallegos <puxerop@gmial.com>
2016-04-23 07:24:22 -0500

    Webpack-production.config.js Is configured for production environment, it's add JSON.stringify and  fix the entry point

--- webpack-production.config.js
+++ webpack-production.config.js
@@ -3,21 +3,26 @@ const webpack = require('webpack')
 const webpackConfig = require('./webpack.config.js')
 
 const PLUGINS = [
+  new webpack.HotModuleReplacementPlugin(),
+  new webpack.NoErrorsPlugin(),
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
   new webpack.optimize.UglifyJsPlugin({
     mangle: false,
     sourcemap: false,
     compress: {
       warnings: false
     }
-  }),
-  new webpack.optimize.DedupePlugin(),
-  new webpack.optimize.OccurenceOrderPlugin(),
+  })
 ]
 
 const WEBPACK_PRODUCTION_CONFIG = Object.assign({}, webpackConfig, {
+  entry: './src/index',
   plugins: PLUGINS,
-  devtool: 'cheap-module-source-map'
+  devtool: 'cheap-module-source-map',
 })
-
 
 module.exports = WEBPACK_PRODUCTION_CONFIG
